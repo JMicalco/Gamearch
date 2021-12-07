0b4be87d7b8e25a60705b27b497dd12146df3517
José Adolfo <jmicalco@gmail.com>
2021-12-06 22:54:30 -0600

    Components Started
    Signed-off-by: José Adolfo <jmicalco@gmail.com>

---
+++ gamearch/src/Styles/Header-style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div``;
+
+export const Content = styled.div``;
+
+export const Logo = styled.img``;
+
+export const IGDBLogoImg = styled.img``;
+
---
+++ gamearch/src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Wrapper, Content, Logo, IGDBLogoImg } from '../Styles/Header-style'
+
+const Header = () => (
+    <Wrapper>
+        <Content>
+            <Logo/>
+            <IGDBLogoImg/>
+        </Content>
+    </Wrapper>
+)
