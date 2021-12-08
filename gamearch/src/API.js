9d24cfd3120a5952a06dba65c3de2dd2bc4ad4c9
José Adolfo <jmicalco@gmail.com>
2021-12-08 17:52:35 -0600

    Setup basics of the API functions to fetch data to the Home component
    Signed-off-by: José Adolfo <jmicalco@gmail.com>

--- gamearch/src/API.js
+++ gamearch/src/API.js
@@ -1,7 +1,16 @@
 const API_URL= "https://api.rawg.io/api/";
 const API_KEY = process.env.REACT_APP_API_KEY;
+const SEARCH_URL = `${API_URL}games?key=${API_KEY}`;
 
-export  {
-    API_URL,
-    API_KEY
+export const FetchGames = async (search, page) =>{
+    if (search) {
+        const searchGame = await fetch(`${SEARCH_URL}&search=${search}&page=${page}`);
+        const json = await searchGame.json();
+        return json
+    } else {
+        const popularGames =  await fetch(`${SEARCH_URL}&ordering=rating&page=${page}`);
+        const json = await popularGames.json();
+        return json
+    } 
+                
 }
--- gamearch/src/components/Home.js
+++ gamearch/src/components/Home.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import NoImage from '../images/no-image.png'
-import { API_URL, API_KEY } from '../API';
+import {FetchGames} from '../API';
+import dotenv from "dotenv";
+dotenv.config();
 
 const Home = () => {
     const [state, setstate] = useState()
@@ -11,25 +13,25 @@ const Home = () => {
         try {
             setError(false)
             setLoading(true)
-
-            const games = await fetch(`${API_URL}games?key=${API_KEY}&page=1`, {
-                method: "GET",
-                headers: new Headers({ 'Content-type': 'application/json'}),
-                mode: 'no-cors'
-            })
-            const json = await games.json()
-            console.log(json.results)
-
-
-            
+            const games = await FetchGames(search, page);
+            setstate(prev =>({
+                ...games,
+                results:
+                page > 1 ? [...prev.results, ...games.results]: [games.results]
+            }))
+                    
         } catch (error) {
             setError(true)
         }
+        setLoading(false)
+
     }
 
     useEffect(() => {
         fetchGames(1)
     }, [])
+
+    console.log(state)
 
 return (
         <div>
